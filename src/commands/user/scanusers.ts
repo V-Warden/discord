490aaea28a00541b9cff5d4445f1892fea6caf57
‮ <me@mk3ext.dev>
2022-05-30 19:04:59 +0100

    fix(scanusers): add catch
    Signed-off-by: ‮ <me@mk3ext.dev>

--- src/commands/user/scanusers.ts
+++ src/commands/user/scanusers.ts
@@ -25,88 +25,101 @@ export default class ScanUsers extends SlashCommand {
         }
 
         const begin = Date.now();
-        interaction.guild.members.fetch().then(async () => {
-            const settings = await client.db.guild.findUnique({
-                where: { id: interaction.guild.id },
-                select: { logChannel: true, punishments: true },
-            });
+        interaction.guild.members
+            .fetch()
+            .then(async () => {
+                const settings = await client.db.guild.findUnique({
+                    where: { id: interaction.guild.id },
+                    select: { logChannel: true, punishments: true },
+                });
 
-            if (!settings) {
-                client.logger.error(
-                    `${this.constructor.name}: ${interaction.guild.name} - Unknown guild - Owner is: ${interaction.guild.ownerId}`
+                if (!settings) {
+                    client.logger.error(
+                        `${this.constructor.name}: ${interaction.guild.name} - Unknown guild - Owner is: ${interaction.guild.ownerId}`
+                    );
+                    return false;
+                }
+
+                if (!settings.punishments.enabled) {
+                    sendEmbed({
+                        interaction,
+                        embed: {
+                            description:
+                                '`🔴` Actioning is disabled on this server, you can enable it using `/config`',
+                            color: Colours.RED,
+                        },
+                    });
+                    return false;
+                }
+
+                client.logger.debug(
+                    `${this.constructor.name}: ${interaction.guild.name} - Initiated by ${interaction.user.id}`
                 );
-                return false;
-            }
 
-            if (!settings.punishments.enabled) {
                 sendEmbed({
                     interaction,
                     embed: {
                         description:
-                            '`🔴` Actioning is disabled on this server, you can enable it using `/config`',
-                        color: Colours.RED,
+                            "Now scanning users. This may take awhile so be patient.\nBe aware this is resource intensive, and shouldn't be used often.\nAbuse of this command will result in punishment.",
+                        color: Colours.YELLOW,
                     },
                 });
-                return false;
-            }
 
-            client.logger.debug(
-                `${this.constructor.name}: ${interaction.guild.name} - Initiated by ${interaction.user.id}`
-            );
-
-            sendEmbed({
-                interaction,
-                embed: {
-                    description:
-                        "Now scanning users. This may take awhile so be patient.\nBe aware this is resource intensive, and shouldn't be used often.\nAbuse of this command will result in punishment.",
-                    color: Colours.YELLOW,
-                },
-            });
-
-            // Reduce database calls from one per member to one
-            // Bulk grab all blacklisted then check if exists
-            // Rather than checking database per member
-            const ids = interaction.guild.members.cache.filter(u => !u.user.bot).map(u => u.id);
-            const users = await client.db.users.findMany({
-                where: {
-                    status: {
-                        in: [UserStatus.BLACKLISTED, UserStatus.PERM_BLACKLISTED],
+                // Reduce database calls from one per member to one
+                // Bulk grab all blacklisted then check if exists
+                // Rather than checking database per member
+                const ids = interaction.guild.members.cache.filter(u => !u.user.bot).map(u => u.id);
+                const users = await client.db.users.findMany({
+                    where: {
+                        status: {
+                            in: [UserStatus.BLACKLISTED, UserStatus.PERM_BLACKLISTED],
+                        },
+                        id: {
+                            in: ids,
+                        },
                     },
-                    id: {
-                        in: ids,
+                });
+
+                await Promise.all(
+                    users.map(async user => {
+                        client.actioning.doAction(
+                            user,
+                            settings.logChannel,
+                            settings.punishments,
+                            interaction.guild.members.cache.get(user.id),
+                            false,
+                            false
+                        );
+                    })
+                );
+                const actioned = users.length === 0 ? 0 : users.length - 1;
+                sendEmbed({
+                    channel: interaction.channel,
+                    embed: {
+                        description: `Scanning completed, \`${actioned} users\` have been actioned`,
+                        color: Colours.GREEN,
                     },
-                },
+                }).catch(e => console.log(e));
+
+                const end = Date.now();
+                client.logger.info(
+                    `${this.constructor.name}: ${
+                        interaction.guild.name
+                    } - actioned ${actioned} users, took ${(end - begin) / 1000}s`
+                );
+                return true;
+            })
+            .catch(e => {
+                client.logger.error(e);
+                sendEmbed({
+                    channel: interaction.channel,
+                    embed: {
+                        description:
+                            'An unexpected error has occured. If re-trying this command does not work, please open a support ticket in the Official Warden Discord',
+                        color: Colours.RED,
+                    },
+                }).catch(e => console.log(e));
             });
-
-            await Promise.all(
-                users.map(async user => {
-                    client.actioning.doAction(
-                        user,
-                        settings.logChannel,
-                        settings.punishments,
-                        interaction.guild.members.cache.get(user.id),
-                        false,
-                        false
-                    );
-                })
-            );
-            const actioned = users.length === 0 ? 0 : users.length - 1;
-            sendEmbed({
-                channel: interaction.channel,
-                embed: {
-                    description: `Scanning completed, \`${actioned} users\` have been actioned`,
-                    color: Colours.GREEN,
-                },
-            }).catch(e => console.log(e));
-
-            const end = Date.now();
-            client.logger.info(
-                `${this.constructor.name}: ${
-                    interaction.guild.name
-                } - actioned ${actioned} users, took ${(end - begin) / 1000}s`
-            );
-            return true;
-        });
 
         return true;
     }
