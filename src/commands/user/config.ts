864ce1f2594cd735e1485e5266abbf29aba6b4e6
‮ <me@mk3ext.dev>
2022-09-11 13:21:38 +0100

    fix(config): temp fix for setting log channel
    Signed-off-by: ‮ <me@mk3ext.dev>

--- src/commands/user/config.ts
+++ src/commands/user/config.ts
@@ -24,50 +24,52 @@ export default class ConfigCommand extends SlashCommand {
 
     public async run(client: Bot, interaction: CommandInteraction): Promise<boolean> {
         const logchan = interaction.options.get('logchan')?.value as string;
+
         if (logchan) {
-            const chan = await interaction.guild.channels.fetch(logchan);
-
-            if (!chan) {
-                await interaction.reply({
-                    ephemeral: true,
-                    embeds: [
-                        {
-                            description: 'Unknown Channel ID',
-                            color: Colours.RED,
-                        },
-                    ],
-                });
-                return false;
-            }
-
-            await client.db.guild
-                .update({
-                    where: { id: interaction.guild.id },
-                    data: { logChannel: logchan },
-                })
+            interaction.guild.channels
+                .fetch(logchan)
                 .then(async () => {
-                    await interaction.reply({
-                        ephemeral: true,
-                        embeds: [
-                            {
-                                description:
-                                    'Successfully updated the log channel, make sure I can `SEND_MESSAGES` in that channel',
-                                color: Colours.GREEN,
-                            },
-                        ],
-                    });
+                    await client.db.guild
+                        .update({
+                            where: { id: interaction.guild.id },
+                            data: { logChannel: logchan },
+                        })
+                        .then(async () => {
+                            await interaction.reply({
+                                ephemeral: true,
+                                embeds: [
+                                    {
+                                        description:
+                                            'Successfully updated the log channel, make sure I can `SEND_MESSAGES` in that channel',
+                                        color: Colours.GREEN,
+                                    },
+                                ],
+                            });
+                        })
+                        .catch(async () => {
+                            await interaction.reply({
+                                ephemeral: true,
+                                embeds: [
+                                    {
+                                        description:
+                                            'An unexpected error has occured, please open a support ticket in Warden discord',
+                                        color: Colours.RED,
+                                    },
+                                ],
+                            });
+                        });
                 })
                 .catch(async () => {
                     await interaction.reply({
                         ephemeral: true,
                         embeds: [
                             {
-                                description:
-                                    'An unexpected error has occured, please open a support ticket in Warden discord',
+                                description: 'Unknown Channel ID',
                                 color: Colours.RED,
                             },
                         ],
                     });
+                    return false;
                 });
         } else {
             await interaction.reply({ ephemeral: true, content: 'Sent configuration menu' });
