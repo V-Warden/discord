2037b00d964546fa6774fe2d570c76d500139a63
‮ <me@mk3ext.dev>
2022-10-23 15:40:44 +0100

    chore(Actioning): try catch
    Signed-off-by: ‮ <me@mk3ext.dev>

--- src/classes/Actioning.ts
+++ src/classes/Actioning.ts
@@ -76,159 +76,165 @@ export class ActionUser {
       return;
     }
 
-    if (realCount === 0) {
-      if (user.status !== 'APPEALED') {
-        await this.client.db.imports.updateMany({
-          where: { id: user.id },
-          data: { appealed: false },
-        });
+    try {
+      if (realCount === 0) {
+        if (user.status !== 'APPEALED') {
+          await this.client.db.imports.updateMany({
+            where: { id: user.id },
+            data: { appealed: false },
+          });
 
-        this.doAction(user, logChannel, punishments, member, toDM, process, rescan);
+          this.doAction(user, logChannel, punishments, member, toDM, process, rescan);
+        }
+        return;
       }
-      return;
-    }
-    const author = {
-      name: `${member.user.username}#${member.user.discriminator} / ${member.id}`,
-      icon_url: member.displayAvatarURL(),
-    };
+      const author = {
+        name: `${member.user.username}#${member.user.discriminator} / ${member.id}`,
+        icon_url: member.displayAvatarURL(),
+      };
 
-    if (toDM) {
-      await member
-        .createDM()
-        .then((chan) => {
-          chan
-            .send({
-              content: `:shield: Warden
+      if (toDM) {
+        await member
+          .createDM()
+          .then((chan) => {
+            chan
+              .send({
+                content: `:shield: Warden
 You are being automodded by ${member.guild.name} for being associated with ${realCount} leaking, cheating or reselling discord servers.
 You may attempt to appeal this via the Official Warden Discord:
 https://discord.gg/jeFeDRasfs`,
-            })
-            .catch(() =>
-              this.client.logger.warn(
-                `${this.constructor.name}: ${member.guild.name} - Unable to send message to ${member.id}`
-              )
-            );
-        })
-        .catch(() => {
+              })
+              .catch(() =>
+                this.client.logger.warn(
+                  `${this.constructor.name}: ${member.guild.name} - Unable to send message to ${member.id}`
+                )
+              );
+          })
+          .catch(() => {
+            sendEmbed({
+              channel,
+              embed: {
+                description: `\`:warning:\` Unable to Direct Message User <@${member.id}>`,
+                author,
+                color: Colours.RED,
+              },
+            }).catch();
+          });
+      }
+
+      if (toDo === 'WARN') {
+        // Stop warning messages on main discord
+        if (punishments.id === '860760302227161118') return;
+        if (!this.client.hasNoPerms(punishments.id, noServerPerms.SEND_MESSAGE) && !rescan) {
+          this.client.logger.debug(
+            `${this.constructor.name}: ${member.guild.name} - ${user.last_username} (${user.id}) - ${toDo}`
+          );
           sendEmbed({
             channel,
             embed: {
-              description: `\`:warning:\` Unable to Direct Message User <@${member.id}>`,
+              description: `:warning: User ${user.last_username} (${
+                member.id
+              }) has been seen in ${realCount} bad discord servers.\n**User Status**: ${user.status.toLowerCase()} / **User Type**: ${user.type.toLowerCase()}`,
               author,
-              color: Colours.RED,
+              color: Colours.GREEN,
             },
-          }).catch();
-        });
-    }
-
-    if (toDo === 'WARN') {
-      // Stop warning messages on main discord
-      if (punishments.id === '860760302227161118') return;
-      if (!this.client.hasNoPerms(punishments.id, noServerPerms.SEND_MESSAGE) && !rescan) {
-        this.client.logger.debug(
-          `${this.constructor.name}: ${member.guild.name} - ${user.last_username} (${user.id}) - ${toDo}`
-        );
-        sendEmbed({
-          channel,
-          embed: {
-            description: `:warning: User ${user.last_username} (${
-              member.id
-            }) has been seen in ${realCount} bad discord servers.\n**User Status**: ${user.status.toLowerCase()} / **User Type**: ${user.type.toLowerCase()}`,
-            author,
-            color: Colours.GREEN,
-          },
-        })
-          .then(() => {
-            if (punishments.roleId)
-              member.roles.add(punishments.roleId, `User Type: ${user.type.toLowerCase()}`).catch(() => {
-                this.client.logger.debug(`Role not found ${punishments.roleId} - ${punishments.id}`);
-              });
-            if (process) this.success.warnings += 1;
           })
-          .catch(() => {
-            this.client.addNoPerms(punishments.id, noServerPerms.SEND_MESSAGE);
-            this.client.logger.warn(
-              `${this.constructor.name}: ${member.guild.name} - Unable to send warning embed, no permissions?`
-            );
-          });
-      } else {
-        if (process) this.failed.warnings += 1;
-      }
-    } else {
-      const realType = toDo === 'BAN' ? noServerPerms.BAN : noServerPerms.KICK;
-      if (!this.client.hasNoPerms(punishments.id, realType)) {
-        let action;
-        if (toDo === 'BAN') {
-          action = member.ban({ reason: `Warden - User Type ${user.type}` });
-        } else if (toDo === 'KICK') {
-          action = member.kick(`Warden - User Type ${user.type}`);
+            .then(() => {
+              if (punishments.roleId)
+                member.roles.add(punishments.roleId, `User Type: ${user.type.toLowerCase()}`).catch(() => {
+                  this.client.logger.debug(`Role not found ${punishments.roleId} - ${punishments.id}`);
+                });
+              if (process) this.success.warnings += 1;
+            })
+            .catch(() => {
+              this.client.addNoPerms(punishments.id, noServerPerms.SEND_MESSAGE);
+              this.client.logger.warn(
+                `${this.constructor.name}: ${member.guild.name} - Unable to send warning embed, no permissions?`
+              );
+            });
         } else {
-          action = null;
+          if (process) this.failed.warnings += 1;
         }
-        action
-          .then(() => {
-            if (!this.client.hasNoPerms(punishments.id, noServerPerms.SEND_MESSAGE)) {
-              sendEmbed({
-                channel,
-                embed: {
-                  description: `:shield: User ${user.last_username} (${
-                    member.id
-                  }) has been punished with a ${toDo}.\nThey have been seen in ${realCount} bad discord servers.\n**User Status**: ${user.status.toLowerCase()}`,
-                  author,
-                  color: Colours.GREEN,
-                },
-              })
-                .then(() => {
-                  if (toDo === 'BAN') {
-                    if (process) this.success.bans += 1;
-                  } else {
-                    if (process) this.success.kicks += 1;
-                  }
+      } else {
+        const realType = toDo === 'BAN' ? noServerPerms.BAN : noServerPerms.KICK;
+        if (!this.client.hasNoPerms(punishments.id, realType)) {
+          let action;
+          if (toDo === 'BAN') {
+            action = member.ban({ reason: `Warden - User Type ${user.type}` });
+          } else if (toDo === 'KICK') {
+            action = member.kick(`Warden - User Type ${user.type}`);
+          } else {
+            action = null;
+          }
+          action
+            .then(() => {
+              if (!this.client.hasNoPerms(punishments.id, noServerPerms.SEND_MESSAGE)) {
+                sendEmbed({
+                  channel,
+                  embed: {
+                    description: `:shield: User ${user.last_username} (${
+                      member.id
+                    }) has been punished with a ${toDo}.\nThey have been seen in ${realCount} bad discord servers.\n**User Status**: ${user.status.toLowerCase()}`,
+                    author,
+                    color: Colours.GREEN,
+                  },
                 })
-                .catch(() => {
-                  this.client.addNoPerms(punishments.id, noServerPerms.SEND_MESSAGE);
+                  .then(() => {
+                    if (toDo === 'BAN') {
+                      if (process) this.success.bans += 1;
+                    } else {
+                      if (process) this.success.kicks += 1;
+                    }
+                  })
+                  .catch(() => {
+                    this.client.addNoPerms(punishments.id, noServerPerms.SEND_MESSAGE);
+                    this.client.logger.warn(
+                      `${this.constructor.name}: ${member.guild.name} - Unable to create message in ${channel?.id}`
+                    );
+                  });
+              }
+
+              this.client.logger.debug(
+                `${this.constructor.name}: ${member.guild.name} - ${user.last_username} (${user.id}) - ${toDo}`
+              );
+            })
+            .catch((e) => {
+              this.client.addNoPerms(punishments.id, realType);
+              if (toDo === 'BAN') {
+                if (process) this.failed.bans += 1;
+              } else {
+                if (process) this.failed.kicks += 1;
+              }
+
+              if (!this.client.hasNoPerms(punishments.id, noServerPerms.SEND_MESSAGE)) {
+                sendEmbed({
+                  channel,
+                  embed: {
+                    description: `:warning: I tried to ${toDo} ${user.last_username} (${member.id}) but something errored!\nPlease verify I have this permission, and am a higher role than this user!`,
+                    author,
+                    color: Colours.RED,
+                  },
+                }).catch(() =>
                   this.client.logger.warn(
                     `${this.constructor.name}: ${member.guild.name} - Unable to create message in ${channel?.id}`
-                  );
-                });
-            }
+                  )
+                );
+              }
 
-            this.client.logger.debug(
-              `${this.constructor.name}: ${member.guild.name} - ${user.last_username} (${user.id}) - ${toDo}`
-            );
-          })
-          .catch((e) => {
-            this.client.addNoPerms(punishments.id, realType);
-            if (toDo === 'BAN') {
-              if (process) this.failed.bans += 1;
-            } else {
-              if (process) this.failed.kicks += 1;
-            }
-
-            if (!this.client.hasNoPerms(punishments.id, noServerPerms.SEND_MESSAGE)) {
-              sendEmbed({
-                channel,
-                embed: {
-                  description: `:warning: I tried to ${toDo} ${user.last_username} (${member.id}) but something errored!\nPlease verify I have this permission, and am a higher role than this user!`,
-                  author,
-                  color: Colours.RED,
-                },
-              }).catch(() =>
-                this.client.logger.warn(
-                  `${this.constructor.name}: ${member.guild.name} - Unable to create message in ${channel?.id}`
-                )
+              this.client.logger.warn(
+                `${this.constructor.name}: ${member.guild.name} - ${user.id} - ${e} - ${realType}`
               );
-            }
-
-            this.client.logger.warn(`${this.constructor.name}: ${member.guild.name} - ${user.id} - ${e} - ${realType}`);
-          });
-      } else {
-        if (toDo === 'BAN') {
-          if (process) this.failed.bans += 1;
+            });
         } else {
-          if (process) this.failed.kicks += 1;
+          if (toDo === 'BAN') {
+            if (process) this.failed.bans += 1;
+          } else {
+            if (process) this.failed.kicks += 1;
+          }
         }
       }
+    } catch (e) {
+      this.client.logger.warn(e);
     }
   }
 
