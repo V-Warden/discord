5d4225da5eaa4bb4c34aad4d1df8ff8c8bd8c363
‮ <me@mk3ext.dev>
2022-12-17 18:40:52 +0000

    fix(ping): remove debug
    Signed-off-by: ‮ <me@mk3ext.dev>

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -4,8 +4,6 @@ export default new Command({
     name: 'ping',
     description: 'replies with pong',
     run: async ({ interaction, client }) => {
-        client.shard?.send({ action: 'actionGlobal' });
-
         return interaction.followUp('Pong3');
     },
 });
