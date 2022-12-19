f0bda9e3df6a07d57322d5e45c07dfd2c1514cba
‮ <me@mk3ext.dev>
2022-12-19 19:34:02 +0000

    fix(messages/sendEmbed): try-catch for invalid log channels
    Signed-off-by: ‮ <me@mk3ext.dev>

--- src/utils/messages/sendEmbed.ts
+++ src/utils/messages/sendEmbed.ts
@@ -1,5 +1,6 @@
 import { Message } from 'discord.js';
 import { sendEmbedOptions } from '../../@types';
+import logger from '../logger';
 
 export default async function ({ interaction, channel, content, embed, components, ephemeral }: sendEmbedOptions): Promise<Message> {
     const sendOpts: any = {
@@ -7,16 +8,24 @@ export default async function ({ interaction, channel, content, embed, component
         components,
         ephemeral: ephemeral ?? false
     };
-
-    if (content) sendOpts.content = content;
-    if (channel) return await channel.send(sendOpts);
-    if (interaction) {
-        if (interaction.deferred || interaction.replied) {
-            return await interaction.editReply(sendOpts)
-        } else {
-            return await interaction.reply(sendOpts)
+    try {
+        if (content) sendOpts.content = content;
+        if (channel) return await channel.send(sendOpts);
+        if (interaction) {
+            if (interaction.deferred || interaction.replied) {
+                return await interaction.editReply(sendOpts)
+            } else {
+                return await interaction.reply(sendOpts)
+            }
         }
-    }
 
-    throw new Error('Interaction or channel not set');
+        throw new Error('Interaction or channel not set');
+    } catch (e) {
+        logger.error({
+            labels: { action: 'sendEmbed', channelId: channel?.id },
+            message: e
+        })
+
+        throw new Error('Invalid channel');
+    }
 }
