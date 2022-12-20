53c57ee52da474c9e4a3a2fd06bdaf6b4a610794
‮ <me@mk3ext.dev>
2022-12-20 15:20:02 +0000

    feat(messages/sendInfo): optional ephermeral
    Signed-off-by: ‮ <me@mk3ext.dev>

--- src/utils/messages/sendInfo.ts
+++ src/utils/messages/sendInfo.ts
@@ -1,26 +1,33 @@
 import { CommandInteraction, Message } from 'discord.js';
 import { Colours } from '../../@types/Colours';
-import logger from '../logger';
 import sendEmbed from './sendEmbed';
 
-export function sendSuccess(interaction: CommandInteraction, message: string): Promise<Message> {
+export function sendSuccess(
+    interaction: CommandInteraction,
+    message: string,
+    ephemeral?: boolean
+): Promise<Message> {
     return sendEmbed({
         interaction,
         embed: {
             description: `\`🟢\` ${message}`,
             color: Colours.GREEN,
         },
-        ephemeral: true,
+        ephemeral: ephemeral ?? true,
     });
 }
 
-export function sendError(interaction: CommandInteraction, message: string): Promise<Message> {
+export function sendError(
+    interaction: CommandInteraction,
+    message: string,
+    ephemeral?: boolean
+): Promise<Message> {
     return sendEmbed({
         interaction,
         embed: {
             description: `\`🔴\` ${message}`,
             color: Colours.RED,
         },
-        ephemeral: true,
+        ephemeral: ephemeral ?? true,
     });
 }
