18a10b53d968a01684650a59a0d4f1ea1e22bc65
‮ <me@mk3ext.dev>
2023-04-30 16:07:52 +0100

    feat(dev/scanappealed): ability to remove appealed users bans from specific guild
    Signed-off-by: ‮ <me@mk3ext.dev>

---
+++ src/commands/dev/scanappealed.ts
@@ -0,0 +1,98 @@
+import { ApplicationCommandOptionType, ChannelType, TextChannel } from 'discord.js';
+import { Command } from '../../structures/Command';
+import db from '../../utils/database';
+import { sendError, sendSuccess } from '../../utils/messages';
+import logger from '../../utils/logger';
+
+export default new Command({
+    name: 'scanappealed',
+    description: 'Removes all bans in a specified guild for appealed users (fixes legacy)',
+    main: true,
+    defaultMemberPermissions: 'Administrator',
+    options: [
+        {
+            type: ApplicationCommandOptionType.String,
+            name: 'id',
+            description: 'Guild ID',
+            required: true,
+        },
+    ],
+    run: async ({ interaction, client }) => {
+        const id = interaction.options.get('id')?.value as string;
+        if (!client.shard) {
+            logger.warn({
+                labels: { userId: id },
+                message: 'No shards online, unable to action appeal',
+            });
+            return sendError(interaction, 'No shards online, unable to run this command');
+        }
+
+        const settings = await db.getGuild({ id: id }, { punishments: true });
+        if (!settings) return sendError(interaction, 'Unable to find guild in the database');
+
+        const result = await client.shard.broadcastEval(
+            async (c, { guildId }) => {
+                await c.guilds.fetch();
+                let message = '';
+
+                const guild = c.guilds.cache.find(x => x.id === guildId);
+                if (!guild) return { guild: false };
+
+                await guild.bans.fetch();
+                await guild.members.fetch();
+
+                const bans = guild.bans.cache.map(x => {
+                    return { id: x.user.id, reason: x.reason };
+                });
+
+                const bannedUsers: string[] = [];
+
+                for (let index = 0; index < bans.length; index++) {
+                    const ban = bans[index];
+                    if (!ban.reason) continue;
+                    if (ban.reason.includes('Warden - User Type')) {
+                        bannedUsers.push(ban.id);
+                    }
+                }
+
+                const users = await db.getManyUsers({ id: { in: bannedUsers }, status: 'APPEALED' });
+                if (users.length === 0) {
+                    message = 'Guild has no people appealed who are banned';
+                    return { error: true, message: message, guild: true };
+                }
+
+                let count = 0;
+                for (let index = 0; index < users.length; index++) {
+                    const user = users[index];
+                    try {
+                        await guild.bans.remove(user.id, 'Appealed');
+                        count += 1;
+                    } catch (e) {
+                        logger.warn({
+                            labels: { command: 'scanappealed', guildId: id },
+                            message: e,
+                        });
+                    }
+                }
+
+                message = `Unbanned ${count} users`;
+                if (users.length - count > 0)
+                    message = `${message}, ${users.length - count} were unable to be unbanned`;
+
+                return { error: false, message: message, guild: true };
+            },
+            { context: { guildId: id } }
+        );
+
+        for (let index = 0; index < result.length; index++) {
+            for (let i = 0; i < result.length; i++) {
+                if (!result[i].guild) continue;
+
+                if (result[i].error) return sendError(interaction, `${result[i].message}`);
+                sendSuccess(interaction, `${result[i].message}`);
+            }
+        }
+
+        return false;
+    },
+});
