d45c05adeb3eb5d66135ed440f681be4019eb970
iiTranquility <21291440+iiTranquility@users.noreply.github.com>
2024-02-24 02:29:22 +0000

    Updated /about invite to new invite

--- src/commands/info/about.ts
+++ src/commands/info/about.ts
@@ -14,7 +14,7 @@ export default new Command({
                           You can call me Warden or 5 Warden (V Warden).
                           \nI was created by Vampire#8144 in an effort to combat the prevalence of pirated code and cheating within the FiveM community.
                           \nI am the frontend for a database of users in Leaking and Cheating Discord servers, with settings to prevent those users from entering your discord server.
-                          \nYou can join the Official Discord for more information: <https://discord.gg/jeFeDRasfs>
+                          \nYou can join the Official Discord for more information: <https://discord.com/invite/MVNZR73Ghf>
                           \nFeel free to contribute here: <https://github.com/V-Warden/discord>`,
                 color: Colours.GREEN,
             },
