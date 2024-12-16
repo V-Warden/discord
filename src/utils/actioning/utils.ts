55572e21a9b94d1d3880b26dd10e5be0a256bde5
Alexander <alexander.bekkerin@zigt.nl>
2024-12-16 17:01:08 +0100

    feat(punish): Add bad server type check

--- src/utils/actioning/utils.ts
+++ src/utils/actioning/utils.ts
@@ -1,24 +1,55 @@
-import { Punish, Punishments, UserType } from '@prisma/client';
+import { Punish, Punishments, UserType } from '@prisma/client'
 
-export function getPunishment(type: UserType, punishments: Punishments): Punish {
-    let toDo: Punish = 'WARN';
-    switch (type) {
-        case UserType.OWNER:
-            toDo = punishments.owner;
-            break;
-        case UserType.SUPPORTER:
-            toDo = punishments.supporter;
-            break;
-        case UserType.LEAKER:
-            toDo = punishments.leaker;
-            break;
-        case UserType.CHEATER:
-            toDo = punishments.cheater;
-            break;
-        case UserType.OTHER:
-            toDo = punishments.other;
-            break;
+export function getPunishment(userType: UserType, punishments: Punishments, BadServersType: string[]): Punish {
+    const punishmentOrder: Punish[] = ['WARN', 'ROLE', 'KICK', 'BAN']
+
+    // Get the punishment for the userType
+    let toDo: Punish = 'WARN'
+    if (userType === UserType.OWNER) {
+        toDo = punishments.owner
+    } else if (userType === UserType.SUPPORTER) {
+        toDo = punishments.supporter
+    } else if (userType === UserType.LEAKER) {
+        toDo = punishments.leaker
+    } else if (userType === UserType.CHEATER) {
+        toDo = punishments.cheater
+    } else if (userType === UserType.OTHER) {
+        toDo = punishments.other
     }
 
-    return toDo;
+    // Get the highest punishment from BadServersType
+    for (const type of BadServersType) {
+        let punishment: Punish = 'WARN'
+        if (type === 'OWNER') {
+            punishment = punishments.owner
+        } else if (type === 'SUPPORTER') {
+            punishment = punishments.supporter
+        } else if (type === 'LEAKER') {
+            punishment = punishments.leaker
+        } else if (type === 'CHEATER') {
+            punishment = punishments.cheater
+        } else if (type === 'OTHER') {
+            punishment = punishments.other
+        }
+
+        // Compare and get the highest punishment
+        if (punishmentOrder.indexOf(punishment) > punishmentOrder.indexOf(toDo)) {
+            toDo = punishment
+        }
+    }
+
+    return toDo
+}
+
+export function convertServersTypeToUsersType(BadServersType: string[]): UserType[] {
+    return BadServersType.map(type => {
+        if (type === 'CHEATING') {
+            return 'CHEATER'
+        } else if (type === 'RESELLING' || type === 'ADVERTISING' || type === 'OTHER') {
+            return 'OTHER'
+        } else if (type === 'LEAKING') {
+            return 'LEAKER'
+        }
+        return type as UserType
+    })
 }
